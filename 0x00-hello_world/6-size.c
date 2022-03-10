c99ab1279505d1e284a1f81e282c46e93d3032db
Nyota254 <knyota66@gmail.com>
2022-03-10 12:40:22 -0800

    fix documentation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
+/**
+ * This function prints size of elements in machine
+ */
+
 int main(void)
 {
-	/**
-	 * This program prints out size of computer its being run on
-	 *
-	 */
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
