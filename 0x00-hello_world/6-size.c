bd54b11a139b0097ebfe70fc4bb5e3b5f26c89f9
Nyota254 <knyota66@gmail.com>
2022-03-10 12:34:50 -0800

    add program to check sizeof c-lang

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*
+	 * This program prints out size of computer its being run on
+	 *
+	 */
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
