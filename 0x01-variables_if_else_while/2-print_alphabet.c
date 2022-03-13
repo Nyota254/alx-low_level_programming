e867c6af3bb277f38ed232308f323adf4dfbedb6
Nyota254 <knyota66@gmail.com>
2022-03-13 05:41:59 -0700

    add alphabet printer

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints out the alphabet
+ *
+ * return: 0 after successful execution
+ */
+
+int main(void)
+{
+	char i = 'a';
+	for (i = 'a';i <= 'z';i++)
+	{
+		putchar(i);
+	}
+	printf("\n");
+	return (0);
+}
