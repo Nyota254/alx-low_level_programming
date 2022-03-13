799412df09e2e5f7750720055b9850dac1ef7ee9
Nyota254 <knyota66@gmail.com>
2022-03-13 06:44:47 -0700

    add number printer

---
+++ 0x01-variables_if_else_while/5-print-numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints 0-9
+ *
+ * Return: 0 after successful execution
+ */
+
+int main(void)
+{
+	int i = 0;
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d",i);
+	}
+	printf("\n");
+	return (0);
+}
