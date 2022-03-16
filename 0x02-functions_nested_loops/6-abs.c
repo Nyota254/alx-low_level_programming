55a5e2e858bb193e0abdcec711a16fddc21d3089
Nyota254 <knyota66@gmail.com>
2022-03-16 22:31:59 +0300

    add absolute value returner

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - returns an absolute value
+ * 
+ * @n:parameter number to be made absolute
+ * 
+ * Return: returns an absolute value
+ */
+
+int _abs(int n)
+{
+    if(n >= 0)
+        return (n);
+    else
+        return (-n);
+}
