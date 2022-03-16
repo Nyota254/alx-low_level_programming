a247b7f29f808511b96d64fb8557955a37f869be
Nyota254 <knyota66@gmail.com>
2022-03-16 14:56:15 +0300

    add alphabet printer

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - prints alphabet
+ * 
+ * Return: Always 0
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
+
+void print_alphabet(void)
+{
+    char i = 'a';
+    for(i = 'a';i <= 'z';i++)
+    {
+        _putchar(i);
+    }
+    _putchar('\n');
+
+    return (0);
+}
