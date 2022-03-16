29c4878ab2b9dc9c4e94dd9e556df0af374b5431
Nyota254 <knyota66@gmail.com>
2022-03-16 22:48:39 +0300

    add last number printer

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of a given number
+ * 
+ * @n: parameter interger to be checked
+ * 
+ * Return:Returns value of last digit
+ * 
+ */
+
+int print_last_digit(int n)
+{
+    int i = n % 10;
+    return (i);
+}
