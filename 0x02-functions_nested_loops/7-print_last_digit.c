c41ee480727510399f6d7ed28e582967c03dbcaa
Nyota254 <knyota66@gmail.com>
2022-03-16 23:20:57 +0300

    fix last number printer

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,15 +15,18 @@ int print_last_digit(int n)
     if(n < 0)
     {
         i = n % -10;
+        _putchar(i);
         return (i);
     }
     else if(n == 0)
     {
+        _putchar(i);
         return (0);
     }
     else
     {
         i = n % 10;
+        _putchar(i);
         return (i);
     }
 }
