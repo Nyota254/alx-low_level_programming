f10d9e0e242972e187ec7c4df201c75852020e73
Nyota254 <knyota66@gmail.com>
2022-03-22 00:36:53 +0300

    add pointed value updater

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes a value to 98 using pointer
+ * 
+ * @n: pointer value to be changed
+ * 
+ * Return: 0 success
+ * 
+ */
+
+void reset_to_98(int *n)
+{
+    *n = 98;
+    return (0);
+}
