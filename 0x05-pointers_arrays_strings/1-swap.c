e17a0cba5f31351b10dab7cec1d3083218db88da
Nyota254 <knyota66@gmail.com>
2022-03-22 00:47:04 +0300

    add int swappers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps values of two intergers
+ * 
+ * @a:one value to be swapped
+ * @b:second value to be swapped
+ * 
+ * Return: Nothing to return
+ * 
+ */
+
+void swap_int(int *a,int *b)
+{
+    int c;
+    *a = c;
+    *a = *b;
+    *b = c;
+}
