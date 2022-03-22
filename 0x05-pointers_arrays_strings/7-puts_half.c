33ae7f861cb55a1ab57061b0baf8a0be182c1b5e
Nyota254 <knyota66@gmail.com>
2022-03-22 13:07:36 +0300

    add half string printer

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,19 +12,25 @@ void puts_half(char *str)
 {
     int i, j, k;
 
-    i = 0;
+	i = 0;
 
-    while(str[i] != '\0')
-    {
-        i++;
-    }
+	while (str[i] != '\0')
+	{
+		i++;
+	}
 
-    j = i/2;
-
-    for(k = j ; k <= i; k++)
-    {
-        _putchar(str[k]);
-    }
-    
-    _putchar('\n');
+	if (i % 2 == 0)
+	{
+		for (k = i / 2; str[k] != '\0'; k++)
+		{
+			_putchar(str[k]);
+		}
+	} else if (i % 2)
+	{
+		for (j = (i - 1) / 2; j < i - 1; j++)
+		{
+			_putchar(str[j + 1]);
+		}
+	}
+	_putchar('\n');
 }
