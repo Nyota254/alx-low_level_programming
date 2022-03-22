b442bbf5be4b46d5801d1fbab8be20708bc89181
Nyota254 <knyota66@gmail.com>
2022-03-22 11:27:07 +0300

    add reverse string printer

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_rev - prints reverse string
+ * 
+ * @s:string to be reversed
+ * Return:Nothing (void)
+ */
+
+void print_rev(char *s)
+{
+    int counter;
+
+    int num;
+
+    int i;
+
+    counter = 0;
+
+    while(s[counter] != '\0')
+    {
+        counter++;
+    }
+
+    num = counter;
+
+    for(i = num - 1; i >= 0; i--)
+    {
+        _putchar(s[i]);
+    }
+    _putchar('\n');
+}
