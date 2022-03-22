ff01640ef7b1b5761c09ac7ff564b2b871598c01
Nyota254 <knyota66@gmail.com>
2022-03-22 10:18:28 +0300

    add string printer

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str:string to be printed out
+ * Return: nothing (void)
+ * 
+ */
+
+void _puts(char *str)
+{
+    int counter;
+
+    counter = 0;
+
+    while(str[counter] != '\0')
+    {
+        _putchar(str[counter]);
+        counter++;
+    }
+    _putchar('\n');
+}
